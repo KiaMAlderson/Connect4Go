6ca2f62a175eeed9d4e3f2066ffb622a72405390
KiaMAlderson <kmo507@york.ac.uk>
2018-06-21 13:47:48 +0100

    Added comments for readability

--- Connect4.go
+++ Connect4.go
@@ -10,32 +10,32 @@ import (
   "github.com/fatih/color"
 )
 
-//Aliase for ease of use
+//Alias for ease of use
 var p = fmt.Print
 
 //The board has 6 rows and 7 columns
-//Board starting config : {"1", "2", "3", "4", "5", "6", "7"}
-//Done by using populateBoard()
 type Board [6][7]string
 
 func populateBoard(board Board) Board{
+  //Board starting config : {"1", "2", "3", "4", "5", "6", "7"}
+  //Columns labelled initially, then overwritten by play
   for i := 0; i < 6; i++ {
     for j := 0; j < 7; j++ {
       board[i][j] = strconv.Itoa(j+1)
     }
   }
-
   return board
 }
 
 func drawBoard(board Board) {
-  //6 rows, 7 columns
+  //Prints board in a grid function showing most recent state of play
   var hor = " ----------------------------- \n"
   p(hor)
   for i := 0; i < 6; i++ {
     for j := 0; j < 7; j++ {
       toPrint := board[i][j]
       p(" | ")
+      //Prints in either Red or Yellow for increased visability
       if (toPrint == "X") {
         x := color.New(color.FgRed)
         x.Print(toPrint)
@@ -56,14 +56,12 @@ func makeMove(turn string, board Board) Board{
   //simulates dropping to the bottom of the available space
   //will reject if column is full or invalid
 
-
-
-  //flag to check if column is full
+  //Flag to check if column is full
   var flag bool = false
 
   for flag == false {
 
-    //Accepts col to place piece from user, then updates pieces
+    //Accepts col to place piece from user
     var column int64
     reader := bufio.NewReader(os.Stdin)
     p("Please enter a column from 1 to 7 that is available: ")
@@ -72,6 +70,7 @@ func makeMove(turn string, board Board) Board{
     column, _ = strconv.ParseInt(col, 0, 64)
 
     for (column <= 0 || column > 7) {
+      //Prevents illegal input
       p("Please enter a column from 1 to 7 that is available: ")
       col, _ := reader.ReadString('\n')
       col = strings.Trim(col, "\n")
@@ -79,6 +78,7 @@ func makeMove(turn string, board Board) Board{
     }
 
     for (board[0][column - 1] == "X") || (board[0][column - 1] == "O") {
+      //Checks if column is full, rejects if so
       p("Please enter a column from 1 to 7 that is available: ")
       col, _ := reader.ReadString('\n')
       col = strings.Trim(col, "\n")
@@ -86,6 +86,7 @@ func makeMove(turn string, board Board) Board{
     }
 
     for i := 5; i >= 0; i-- {
+      //Simulates dropping the piece down to the bottom of empty space in col
       if (board[i][column - 1] == "X") || (board[i][column - 1] == "O") {
         continue
         } else {
@@ -103,8 +104,7 @@ func checkStatus(board Board) bool {
 
   var flag bool = false
 
-  //j - j+1 - j+2 - j+3
-  // 0 <= j < 4 for rows
+  //Checks all rows for a line of 4
   for i := 0; i < 6; i++ {
     for j := 0; j < 4; j++ {
       if (board[i][j] == board[i][j+1]) && (board[i][j] == board[i][j+2]) && (board[i][j] == board[i][j+3]){
@@ -114,8 +114,7 @@ func checkStatus(board Board) bool {
   }
 
 
-  //i - i+1 - i+2 - i+3
-  // 0 <= i < 3 for columns
+  //Checks all columns for a line of 4
   for i := 0; i < 3; i++ {
     for j := 0; j < 7; j++ {
       if (board[i][j] == board[i+1][j]) && (board[i][j] == board[i+2][j]) &&
@@ -126,8 +125,7 @@ func checkStatus(board Board) bool {
   }
 
 
-  // (i, j) - (i+1, j+1) - (i+2, j+2) - (i+3, j+3)
-  // (0, 0) <= (i, j) < (3, 4) for L-R diagonals \
+  //Checks L-R diagonals for a line of 4
   for i := 0; i < 3; i++ {
     for j := 0; j < 4; j++ {
       if (board[i][j] == board[i+1][j+1]) && (board[i][j] == board[i+2][j+2]) && (board[i][j] == board[i+3][j+3]){
@@ -136,8 +134,7 @@ func checkStatus(board Board) bool {
     }
   }
 
-  // (i, j) - (i+1, j-1) - (i+2, j-2) - (i+3, j-3)
-  // (0, 3) <= (i, j) < (3, 7) for R-L diagonals /
+  //Checks R-L diagonals for a line of 4
   for i := 0; i < 3; i++ {
     for j := 3; j < 7; j++ {
       if (board[i][j] == board[i+1][j-1]) && (board[i][j] == board[i+2][j-2]) && (board[i][j] == board[i+3][j-3]){
@@ -150,6 +147,7 @@ func checkStatus(board Board) bool {
 }
 
 func Even(number int) bool {
+    //Convenience function for checking if a number is even
     return number%2 == 0
 }
 
@@ -160,11 +158,13 @@ func main() {
   var turn string
   board = populateBoard(board)
 
+  //Clears the terminal before play
   p(cursor.ClearEntireScreen())
   p(cursor.MoveTo(0, 0))
 
   p("-----------------------------------------------------------\n")
   color.Red("Welcome to Connect4!\n")
+  //Gathers names from players
   reader := bufio.NewReader(os.Stdin)
   p("X Player 1 name : ")
   name1, _ := reader.ReadString('\n')
@@ -174,6 +174,7 @@ func main() {
   name1 = strings.Trim(name1, "\n")
   name2 = strings.Trim(name2, "\n")
 
+  //Begin game loop. Max of 42 turns
   for i := 0; i < 42; i++ {
     p(cursor.ClearEntireScreen())
     p(cursor.MoveTo(0, 0))
@@ -185,9 +186,11 @@ func main() {
       turn = "O"
     }
 
+    //Draw board again before each new move accepted
     drawBoard(board)
     board = makeMove(turn, board)
 
+    //Check for win state, then exit game loop if true
     if checkStatus(board) {
       drawBoard(board)
       if Even(i){
@@ -195,10 +198,12 @@ func main() {
       } else {
         p("\n" + name2 + " wins!\n\n")
       }
+      //Exit game successfully
       os.Exit(0)
     }
 
   }
+  //Indicates a draw, before finishing game
   p("It's a draw!\n")
   p("Thanks for playing!\n")
 }
